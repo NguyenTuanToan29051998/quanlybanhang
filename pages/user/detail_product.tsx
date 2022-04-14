0d0a46bb6f01065078d1ac1557ee76b8480088ad
NguyenTuanToan29051998 <toandola1998@gmail.com>
2022-04-14 09:18:07 +0700

    update-detail-product

--- pages/user/detail_product.tsx
+++ pages/user/detail_product.tsx
@@ -388,16 +388,48 @@ const DetailProduct: FC = () => {
             alt=""
           />
         </div>
-        <div className="grid grid-cols-5 gap-6">
-          {products.slice(0, 5).map((item, index) => {
-            return (
-              <>
-                <div className="rounded-10px border">
-                  <ProductComponent data={item} index={index + 1} />
-                </div>
-              </>
-            )
-          })}
+        <div className="relative flex items-center">
+          <div className="absolute -left-[4%] z-20 ml-1 grid h-10 w-8 cursor-pointer items-center justify-center rounded border bg-green-3ba66b ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </div>
+          <div className="grid grid-cols-5 gap-6">
+            {products.slice(0, 5).map((item, index) => {
+              return (
+                <>
+                  <div className="rounded-10px border">
+                    <ProductComponent data={item} index={index + 1} />
+                  </div>
+                </>
+              )
+            })}
+          </div>
+          <div className="absolute -right-[4%] z-20 mr-1 grid h-10 w-8 cursor-pointer  items-center justify-center rounded border bg-green-3ba66b">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer text-white "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       <div className="mt-16">
@@ -414,16 +446,48 @@ const DetailProduct: FC = () => {
             alt=""
           />
         </div>
-        <div className="grid grid-cols-5 gap-6">
-          {products.slice(0, 5).map((item, index) => {
-            return (
-              <>
-                <div className="rounded-10px border">
-                  <ProductComponent data={item} index={index + 1} />
-                </div>
-              </>
-            )
-          })}
+        <div className="relative flex items-center">
+          <div className="absolute -left-[4%] z-20 ml-1 grid h-10 w-8 cursor-pointer items-center justify-center rounded border bg-green-3ba66b ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </div>
+          <div className="grid grid-cols-5 gap-6">
+            {products.slice(0, 5).map((item, index) => {
+              return (
+                <>
+                  <div className="rounded-10px border">
+                    <ProductComponent data={item} index={index + 1} />
+                  </div>
+                </>
+              )
+            })}
+          </div>
+          <div className="absolute -right-[4%] z-20 mr-1 grid h-10 w-8 cursor-pointer  items-center justify-center rounded border bg-green-3ba66b">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer text-white "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 5l7 7-7 7"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
